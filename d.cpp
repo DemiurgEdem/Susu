366fc45c4e5c838de94bd31253dbf337c1f4ae46
DemiurgEdem <demiurgbaserman19@gmail.com>
2026-03-13 11:18:55 +0500

    d.cpp

--- d.cpp
+++ d.cpp
@@ -17,21 +17,14 @@ int main()
 		printf("%i. %lf\n", i, w);
 	}*/
 
-	double x = 2;
-	double eps = 0.0000001;
+	double a, x = 2, z = -1, s = 0;
 	int i = 1;
-	double a = -x * x / 4;
-	double s = a;
-	double abs_a = a;
 
-	while (true){
+	a = z * x * x / 4;
 
-		if (abs_a < 0) abs_a = -abs_a;
+	printf("%lf, %lf, %lf", a, z, s);
 
-		if (abs_a <= eps) break;
 
-		a = a * (-x * x * i) / 
-	}
 
 	system("pause");
 }
