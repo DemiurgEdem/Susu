4b5a1ada4ab1cda7c80ffec415269e681758b848
DemiurgEdem <demiurgbaserman19@gmail.com>
2026-05-08 11:26:39 +0500

    Create zanyatie13.cpp

---
+++ zanyatie13.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class electronica {
+	string name;
+	int memoryCapacity;
+public:
+	int deviceCount;
+	int totalMemory;
+
+	electronica(string n, int memory) {
+		name = n;
+		if (memory <= 0) memoryCapacity = 0;
+		else memoryCapacity = memory;
+		deviceCount++;
+		totalMemory += memory;
+	}
+	
+	virtual void show() {
+		cout << "Название устройства: " << name << endl;
+		cout << "Память устройства: " << memoryCapacity << endl;
+	}
+
+	virtual int averageCapacity() {
+		if (deviceCount == 0) return 0;
+		else return totalMemory / deviceCount;
+	}
+};
+
+int main() {
+	class smartphone : public electronica {
+		int camera;
+
+	public:
+		smartphone(
+			string name = "", int memoryCapacity = 0, int camera = 0) : 
+			electronica(name, memoryCapacity) {
+			this->camera = camera < 0 ? 0 : camera;
+		}
+		void show() {
+			electronica::show();
+			cout << "Разрешение камеры: " << camera << endl;
+		}
+	};
+}
