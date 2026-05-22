1bbb00dd97425f0c217722b2a2f0afd13de12bfb
DemiurgEdem <demiurgbaserman19@gmail.com>
2026-05-22 10:04:57 +0500

    Update zanyatie13_a.cpp

--- zanyatie13_a.cpp
+++ zanyatie13_a.cpp
@@ -1,51 +1,77 @@
+#include <iostream>
+using namespace std;
 
 template <class X>
 class Array {
-	X* arr;
-	int size;
+    X* arr;
+    int size;
 
 public:
-	// Конструктор
-	Array(int s = 0) {
-		size = s;
+    // Конструктор
+    Array(int s = 0) {
+        size = s;
 
-		if (size > 0)
-			arr = new X[size];
-		else
-			arr = nullptr;
-	}
+        if (size > 0)
+            arr = new X[size];
+        else
+            arr = nullptr;
+    }
 
-	// Диструктор
-	-Array() {
-		delete[] arr;
-	}
+    // Деструктор
+    ~Array() {
+        delete[] arr;
+    }
 
-	// Взятие размера
-	int getSize() const {
-		return size;
-	}
+    // Перегрузка int() -> размер массива
+    operator int() const {
+        return size;
+    }
 
-	// Индекс
-	X& operator[](int index) {
-		if (index < 0 || index >= size) {
-			count << "Ошибка индекса!" << endl;
-			exit(0);
-		}
+    // Доступ по индексу
+    X& operator[](int index) {
+        if (index < 0 || index >= size) {
+            cout << "Ошибка индекса!" << endl;
+            exit(1);
+        }
 
-		return arr[index];
-	}
+        return arr[index];
+    }
 
-	// Заполение
-	void set(int index, const X& value) {
-		if (index >= 0 && index < size)
-			arr[index] = value;
-	}
+    // Константная версия []
+    const X& operator[](int index) const {
+        if (index < 0 || index >= size) {
+            cout << "Ошибка индекса!" << endl;
+            exit(1);
+        }
 
-	// Вывод
-	void show() const {
-		for (int i = 0; i < size; i++) {
-			arr[i].show();
-			count << endl;
-		}
-	}
+        return arr[index];
+    }
+
+    // Заполнение
+    void set(int index, const X& value) {
+        if (index >= 0 && index < size)
+            arr[index] = value;
+    }
+
+    // Вывод
+    void show() const {
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
 };
+
+int main() {
+    Array<int> a(5);
+
+    for (int i = 0; i < (int)a; i++) {
+        a[i] = i * 10;
+    }
+
+    a.show();
+
+    cout << "Размер массива: " << (int)a << endl;
+
+    return 0;
+}
