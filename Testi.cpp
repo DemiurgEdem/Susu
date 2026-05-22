bfe4d5c96106eb8fbbf6fbb62d39b4d95b685576
DemiurgEdem <demiurgbaserman19@gmail.com>
2026-05-22 12:41:28 +0500

    Create Testi.cpp

---
+++ Testi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	string str = "Привет";
+	int len = str.length();
+	cout << str.substr(len - 3) << endl;
+
+	if (str.substr(len - 3) == "вет") cout << "Прокатит";
+	else "Облом";
+}
