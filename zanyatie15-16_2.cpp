b6dfa1202cf4d1f2cddd99bbb588f01b679c9c83
DemiurgEdem <demiurgbaserman19@gmail.com>
2026-05-30 13:36:31 +0500

    Update zanyatie15-16_2.cpp

--- zanyatie15-16_2.cpp
+++ zanyatie15-16_2.cpp
@@ -1,6 +1,34 @@
 // Подзадание 2
 
+#include <iostream> 
+#include <chrono> 
+#include <Eigen/Dense>
+using namespace std;
+using namespace Eigen;
 
+int main() {
+    auto start = chrono::high_resolution_clock::now();
+
+    Matrix2d A;
+    A << 2, 1,
+        5, 3;
+
+    Matrix2d B;
+    B << 1, 2,
+         4, 3;
+
+    Matrix2d X = A.colPivHouseholderQr().solve(B);
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start;
+    chrono::milliseconds ms = chrono::duration_cast<chrono::milliseconds>(duration);
+
+    cout << "Solution: \n" << X << endl;
+    cout << duration.count() << " s" << endl;
+    cout << ms.count() << " ms" << endl;
+
+    return 0;
+}
 
 // Подзадание 5
 
@@ -11,12 +39,10 @@ using namespace std;
 using namespace Eigen;
 
 int main() {
-    //начало временнОго отрезка 
-    auto start = chrono::high_resolution_clock::now();
-
     int n, m;
     cout << "Enter the dimensions of the matrix n×m: ";
-    cin >> n, m;
+    cin >> n;
+    cin >> m;
     MatrixXd mat(n, m);
     cout << "Enter the numbers for the " << n << "×" << m << " matrix: \n";
     for (int i = 0; i < n; i++) {
@@ -26,15 +52,17 @@ int main() {
         }
     }
 
+    auto start = chrono::high_resolution_clock::now();
+
     MatrixXd trMat = mat.transpose();
-    
+
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> duration = end - start;
     chrono::milliseconds ms = chrono::duration_cast<chrono::milliseconds>(duration);
 
     cout << "Original matrix: \n" << mat << endl;
     cout << "Transpose matrix: \n" << trMat << endl;
-    
+
     cout << duration.count() << " s" << endl;
     cout << ms.count() << " ms" << endl;
 
